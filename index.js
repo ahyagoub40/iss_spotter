b0e0194cf4e1474ae76e8a8c78008ec4408e405b
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-17 03:26:10 +0000

    added the coordinates to the function to give the next flyover

--- index.js
+++ index.js
@@ -9,9 +9,20 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
-const fetchCoordsByIP  = require('./iss');
+// const fetchCoordsByIP  = require('./iss');
 
-fetchCoordsByIP('207.228.85.169',(error, data) => {
+// fetchCoordsByIP('207.228.85.169',(error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , data);
+// });
+
+const fetchISSFlyOverTimes  = require('./iss');
+
+fetchISSFlyOverTimes({ latitude: '43.63190', longitude: '-79.37160' },(error, data) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
--- iss.js
+++ iss.js
@@ -10,10 +10,29 @@
 const request = require('request');
 
 
-const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  // call back takes two parameters
-  request('https://api.ipify.org?format=json', (error, response, body) => {
+// const fetchMyIP = function(callback) {
+//   // use request to fetch IP address from JSON API
+//   // call back takes two parameters
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+//     if (error) {
+//       return callback(error,null);
+//     } else if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     } else {
+//       const data = JSON.parse(body);
+//       const IP = data["ip"];
+//       // console.log(IP);
+//       callback(null,IP);
+//     }
+//   });
+// };
+
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords["latitude"]}&lon=${coords["longitude"]}`, (error, response, body) => {
     if (error) {
       return callback(error,null);
     } else if (response.statusCode !== 200) {
@@ -21,35 +40,36 @@ const fetchMyIP = function(callback) {
       callback(Error(msg), null);
       return;
     } else {
-      const data = JSON.parse(body);
-      const IP = data["ip"];
+      const flyOverObject = JSON.parse(body);
+      const response = flyOverObject["response"];
+     
+      
       // console.log(IP);
-      callback(null,IP);
+      callback(null,response);
     }
   });
 };
+// const fetchCoordsByIP = function(ip, callback) {
+//   request(`https://ipvigilante.com/json/invalidiphere${ip}`, (error, response, body) => {
+//     if (error) {
+//       return callback(error,null);
+//     } else if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     } else {
+//       const IPObject = JSON.parse(body);
+//       const latitude = IPObject["data"]["latitude"];
+//       const longitude = IPObject["data"]["longitude"];
+//       const geoObject = {};
+//       geoObject["latitude"] = latitude;
+//       geoObject["longitude"] = longitude;
+//       // console.log(IP);
+//       callback(null,geoObject);
+//     }
+//   });
+// };
 
-
-
-const fetchCoordsByIP = function(ip, callback) {
-  request(`https://ipvigilante.com/json/invalidiphere${ip}`, (error, response, body) => {
-    if (error) {
-      return callback(error,null);
-    } else if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
-    } else {
-      const IPObject = JSON.parse(body);
-      const latitude = IPObject["data"]["latitude"];
-      const longitude = IPObject["data"]["longitude"];
-      const geoObject = {};
-      geoObject["latitude"] = latitude;
-      geoObject["longitude"] = longitude;
-      // console.log(IP);
-      callback(null,geoObject);
-    }
-  });
-};
-module.exports = fetchMyIP;
-module.exports = fetchCoordsByIP;
+// module.exports = fetchMyIP;
+// module.exports = fetchCoordsByIP;
+module.exports = fetchISSFlyOverTimes;
